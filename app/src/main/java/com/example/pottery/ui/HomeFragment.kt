8ccb639d47b4bfedd754870f0bdd09316db632de
Pareeesa <parisa.faridi80@gmail.com>
2022-05-05 23:36:36 +0430

    Remove unnecessary safe arg action

--- app/src/main/java/com/example/pottery/ui/HomeFragment.kt
+++ app/src/main/java/com/example/pottery/ui/HomeFragment.kt
@@ -1,23 +1,21 @@
 package com.example.pottery.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.example.pottery.R
 import com.example.pottery.adapters.FormulaAdapter
 import com.example.pottery.databinding.FragmentHomeBinding
-import com.example.pottery.room.Formula
-import com.example.pottery.room.Item
 import com.example.pottery.viewModels.FormulaViewModel
 
 class HomeFragment : Fragment() {
 
     private lateinit var binding :FragmentHomeBinding
-    val viewModel: FormulaViewModel by viewModels()
+    private val viewModel: FormulaViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,7 +28,7 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val formulaObserver = viewModel.formulaList?.observe(viewLifecycleOwner) { it ->
+        viewModel.formulaList?.observe(viewLifecycleOwner) { it ->
             if (it != null) {
                 val adapter = FormulaAdapter()
                 binding.recyclerView.adapter = adapter
@@ -38,10 +36,7 @@ class HomeFragment : Fragment() {
             }
         }
         binding.btnAdd.setOnClickListener {
-            viewModel.insert(Formula(0, arrayListOf()))
-            val action = HomeFragmentDirections.actionHomeFragmentToAddFormulaFragment(1)
-            findNavController().navigate(action)
-            //findNavController().navigate(R.id.action_homeFragment_to_addFormulaFragment)
+            findNavController().navigate(R.id.action_homeFragment_to_addFormulaFragment)
         }
     }
 }
