59f7225887cd76b450dabdff4da97d5994f0f411
Pareeesa <parisa.faridi80@gmail.com>
2022-05-05 17:03:27 +0430

    Fix the FormulaViewModel class in order to avoid null exceptions

--- app/src/main/java/com/example/pottery/viewModels/FormulaViewModel.kt
+++ app/src/main/java/com/example/pottery/viewModels/FormulaViewModel.kt
@@ -7,17 +7,17 @@ import com.example.pottery.room.Formula
 import com.example.pottery.room.FormulaRepository
 import com.example.pottery.room.Item
 
-class FormulaViewModel(app: Application) : AndroidViewModel(app) {
-    private var repository = FormulaRepository()
-    var formulaList: LiveData<List<Formula>>
+class FormulaViewModel(application: Application) : AndroidViewModel(application) {
 
-    init {
-        repository.initDB(app.applicationContext)
-        formulaList = repository.formulaList
-    }
-
-    fun addFormula(formula: Formula) {
-        repository.addFormula(formula)
+    private val repository: FormulaRepository = FormulaRepository(application)
+    val formulaList: LiveData<List<Formula>?>? = repository.getAllFormula()
+    /*init {
+        insert(Formula(0,listOf(Item(0,"",0.0))))
+    }*/
+    private fun insert(formula: Formula?) {
+        if (formula != null) {
+            repository.insert(formula)
+        }
     }
 }
 
