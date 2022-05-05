ac8e7278e4cc4cf939e7b953ab177d13a51bb830
Pareeesa <parisa.faridi80@gmail.com>
2022-05-05 14:44:47 +0430

    Connect the adapter and test it by test data

--- app/src/main/java/com/example/pottery/ui/HomeFragment.kt
+++ app/src/main/java/com/example/pottery/ui/HomeFragment.kt
@@ -6,14 +6,32 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.pottery.R
+import com.example.pottery.adapters.FormulaAdapter
+import com.example.pottery.databinding.FragmentHomeBinding
+import com.example.pottery.room.Formula
+import com.example.pottery.room.Item
 
 class HomeFragment : Fragment() {
+
+    private lateinit var binding :FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val adapter = FormulaAdapter()
+        binding.recyclerView.adapter = adapter
+        val list = listOf(
+            Formula(1, listOf(Item(1,"sotb",200.0))),
+            Formula(2, listOf(Item(1,"sotb",200.0))),
+            Formula(3, listOf(Item(1,"sotb",200.0)))
+            )
+        adapter.submitList(list)
     }
 }
