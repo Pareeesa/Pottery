201bd2c1e49d1a0f56a105a154c9d39c22ef8cef
Pareeesa <parisa.faridi80@gmail.com>
2022-05-07 00:10:25 +0430

    Create a live data and observe it Add FormulaViewModel

--- app/src/main/java/com/example/pottery/ui/AddFormulaFragment.kt
+++ app/src/main/java/com/example/pottery/ui/AddFormulaFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.widget.EditText
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.example.pottery.adapters.ItemAdapter
 import com.example.pottery.databinding.FragmentAddFormulaBinding
 import com.example.pottery.room.Formula
 import com.example.pottery.room.Item
@@ -16,6 +17,7 @@ class AddFormulaFragment : Fragment() {
 
     private lateinit var binding: FragmentAddFormulaBinding
     private val viewModel: FormulaViewModel by viewModels()
+    val list : MutableList<Item> = mutableListOf()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,23 +30,31 @@ class AddFormulaFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val adapter = ItemAdapter()
+        viewModel.itemListLiveData.observe(viewLifecycleOwner){
+            binding.recyclerView.adapter = adapter
+            adapter.submitList(it)
+        }
+
         binding.btnAddItem.setOnClickListener {
             if (hasEmptyField()) {
                 checkForErrors()
                 return@setOnClickListener
             }
-            val formula =viewModel.findFormula(binding.etFormulaId.text.toString().toInt())
-            val item = Item(
+            viewModel.addItem(
+                Item(
                 binding.etId.text.toString().toInt(),
                 binding.etMaterial.text.toString(),
-                binding.etAmount.text.toString().toDouble()
+                binding.etAmount.text.toString().toDouble())
             )
-            if (formula != null){
-                viewModel.update(Formula(formula.id,formula.itemList.plus(item)))
-            }else
-                viewModel.insert(Formula(0, arrayListOf(item)))
         }
-
+        binding.btnSave.setOnClickListener {
+            val formula =viewModel.findFormula(binding.etFormulaId.text.toString().toInt())
+            if (formula != null){
+                viewModel.update(Formula(formula.id,formula.itemList.plus(list)))
+            }else
+                viewModel.insert(Formula(0, list))
+        }
     }
     private fun hasEmptyField(): Boolean {
         return (binding.etFormulaId.text.isNullOrEmpty() ||binding.etId.text.isNullOrEmpty() || binding.etMaterial.text.isNullOrEmpty() ||
--- app/src/main/java/com/example/pottery/viewModels/FormulaViewModel.kt
+++ app/src/main/java/com/example/pottery/viewModels/FormulaViewModel.kt
@@ -3,13 +3,22 @@ package com.example.pottery.viewModels
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.example.pottery.room.Formula
 import com.example.pottery.room.FormulaRepository
+import com.example.pottery.room.Item
 
 class FormulaViewModel(application: Application) : AndroidViewModel(application) {
 
     private val repository: FormulaRepository = FormulaRepository(application)
     val formulaList: LiveData<List<Formula>?>? = repository.getAllFormula()
+    val itemListLiveData = MutableLiveData<MutableList<Item>>()
+    private val itemsArrayList = ArrayList<Item>()
+
+    fun addItem(item: Item) {
+        itemsArrayList.add(item)
+        itemListLiveData.value = itemsArrayList
+    }
     /*init {
         insert(Formula(0,listOf(Item(0,"",0.0))))
     }*/
