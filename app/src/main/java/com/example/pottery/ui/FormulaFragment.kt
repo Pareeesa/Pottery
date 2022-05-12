35a694223260a6b83b3fa121dfadff135717a81d
Nick Gharizadeh <nickgharizadeh@gmail.com>
2022-05-12 21:03:16 +0430

    imp/make design of formula fragment

--- app/src/main/java/com/example/pottery/ui/FormulaFragment.kt
+++ app/src/main/java/com/example/pottery/ui/FormulaFragment.kt
@@ -5,9 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.example.pottery.R
+import com.example.pottery.databinding.FragmentAddFormulaBinding
+import com.example.pottery.viewModels.FormulaViewModel
 
 class FormulaFragment : Fragment() {
+    private lateinit var binding: FragmentAddFormulaBinding
+    private val viewModel: FormulaViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -15,5 +20,10 @@ class FormulaFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_formula, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
     }
 }
