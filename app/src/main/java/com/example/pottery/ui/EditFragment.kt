5417749064f452d82a030148777ef51c26c59c45
Pareeesa <parisa.faridi80@gmail.com>
2022-05-12 20:54:17 +0430

    Design editFragment

--- app/src/main/java/com/example/pottery/ui/EditFragment.kt
+++ app/src/main/java/com/example/pottery/ui/EditFragment.kt
@@ -5,22 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.example.pottery.R
+import com.example.pottery.databinding.FragmentEditBinding
+import com.example.pottery.databinding.FragmentHomeBinding
+import com.example.pottery.viewModels.FormulaViewModel
 
 
 class EditFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+    private lateinit var binding: FragmentEditBinding
+    private val viewModel: FormulaViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_edit, container, false)
+        binding = FragmentEditBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 
 
