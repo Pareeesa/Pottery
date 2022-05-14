8c57436e63b4fc8752421bd8d37f16f7ebd27110
Pareeesa <parisa.faridi80@gmail.com>
2022-05-14 12:36:06 +0430

    Add delete button to item view

--- app/src/main/java/com/example/pottery/adapters/ItemAdapter.kt
+++ app/src/main/java/com/example/pottery/adapters/ItemAdapter.kt
@@ -4,13 +4,15 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.pottery.R
 import com.example.pottery.databinding.ItemViewBinding
 import com.example.pottery.room.Item
-import androidx.recyclerview.widget.ListAdapter
 
-class ItemAdapter: ListAdapter<Item, ItemAdapter.ItemHolder>(ItemDiffCallBack) {
+typealias ItemClickHandler = (Item) -> Unit
+
+class ItemAdapter(private val clickHandler: ItemClickHandler): ListAdapter<Item, ItemAdapter.ItemHolder>(ItemDiffCallBack) {
 
     object ItemDiffCallBack: DiffUtil.ItemCallback<Item>() {
         override fun areItemsTheSame(oldItem: Item, newItem: Item): Boolean {
@@ -33,5 +35,8 @@ class ItemAdapter: ListAdapter<Item, ItemAdapter.ItemHolder>(ItemDiffCallBack) {
     }
     override fun onBindViewHolder(holder: ItemHolder, position: Int) {
         holder.binding.item = getItem(position)
+        holder.binding.btnDelete.setOnClickListener {
+            clickHandler.invoke(getItem(position))
+        }
     }
 }
