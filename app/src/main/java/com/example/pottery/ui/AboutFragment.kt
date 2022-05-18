487e4f8096f6a6c66e6f881431bc09ad0f7cbebf
Nick Gharizadeh <nickgharizadeh@gmail.com>
2022-05-18 21:09:57 +0430

    fea/implement about fragment

--- app/src/main/java/com/example/pottery/ui/AboutFragment.kt
+++ app/src/main/java/com/example/pottery/ui/AboutFragment.kt
@@ -1,15 +1,19 @@
 package com.example.pottery.ui
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.pottery.R
+import com.example.pottery.databinding.FragmentAboutBinding
+import com.example.pottery.databinding.FragmentAddFormulaBinding
 
 
 class AboutFragment : Fragment() {
-
+    private lateinit var binding: FragmentAboutBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -19,12 +23,23 @@ class AboutFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false)
+        binding = FragmentAboutBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.imageViewIconAbout.setOnClickListener{
+            openURL()
+        }
+        binding.textViewURL.setOnClickListener{
+            openURL()
+        }
+    }
 
+    fun openURL()
+    {
+        val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("http://tooskawood.ir"))
+        startActivity(browserIntent)
     }
 }
