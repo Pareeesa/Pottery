413041ec95585125f94bfe05917057206f9658f8
Nick Gharizadeh <nickgharizadeh@gmail.com>
2022-07-03 13:23:12 +0430

    fea/ add web view fragment

---
+++ app/src/main/java/com/example/pottery/ui/WebViewFragment.kt
@@ -0,0 +1,43 @@
+package com.example.pottery.ui
+
+import android.annotation.SuppressLint
+import android.os.Build
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebViewClient
+import androidx.annotation.RequiresApi
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import com.example.pottery.databinding.FragmentWebViewBinding
+import com.example.pottery.viewModels.FormulaViewModel
+
+
+class WebViewFragment : Fragment() {
+    private lateinit var binding: FragmentWebViewBinding
+    val formulaViewModel: FormulaViewModel by activityViewModels()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentWebViewBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.webView.webViewClient = WebViewClient()
+        binding.webView.apply {
+            loadUrl(formulaViewModel.webViewURL)
+            settings.javaScriptEnabled = true
+        }
+    }
+}
--- app/src/main/java/com/example/pottery/viewModels/FormulaViewModel.kt
+++ app/src/main/java/com/example/pottery/viewModels/FormulaViewModel.kt
@@ -12,10 +12,11 @@ import com.example.pottery.room.Item
 
 class FormulaViewModel(application: Application) : AndroidViewModel(application) {
 
+    var webViewURL: String = " "
     private val repository: FormulaRepository = FormulaRepository(application)
     val itemListLiveData = MutableLiveData<MutableList<NewItem>>()
     val searchQuery = MutableLiveData("")
-    private val formulaMap =Transformations.switchMap(searchQuery) {
+    private val formulaMap = Transformations.switchMap(searchQuery) {
         repository.searchDb(it)
     }
     val formulaList: LiveData<List<Formula>?> = formulaMap
@@ -23,41 +24,46 @@ class FormulaViewModel(application: Application) : AndroidViewModel(application)
     fun insertFormula(formula: Formula) {
         repository.insert(formula)
     }
-    fun findFormulaByName(name:String): LiveData<FormulaWithItems>? {
+
+    fun findFormulaByName(name: String): LiveData<FormulaWithItems>? {
         return repository.findFormulaByName(name)
     }
 
     fun isNewItemRepeated(item: NewItem): Boolean {
-        if (itemListLiveData.value?.size == null || itemListLiveData.value?.size==0)
+        if (itemListLiveData.value?.size == null || itemListLiveData.value?.size == 0)
             return false
         else if (itemListLiveData.value?.contains(item)!!)
             return true
         return false
     }
-    fun itemIsRepeated(itemCode: String,formulaName:String):Boolean{
-        return repository.itemIsRepeated(itemCode,formulaName)
+
+    fun itemIsRepeated(itemCode: String, formulaName: String): Boolean {
+        return repository.itemIsRepeated(itemCode, formulaName)
     }
-    fun deleteFormula(formula:Formula) {
+
+    fun deleteFormula(formula: Formula) {
         repository.deleteFormula(formula)
         repository.deleteItems(formula.formulaName)
     }
-    fun isFormulaNew(name: String):Boolean{
+
+    fun isFormulaNew(name: String): Boolean {
         return repository.isFormulaNew(name)
     }
 
-    fun insertItems(formulaName:String) {
-        if (itemListLiveData.value != null){
-            for (i in itemListLiveData.value!!){
-                repository.addItem(Item(0,i.code,formulaName,i.material,i.amount))
+    fun insertItems(formulaName: String) {
+        if (itemListLiveData.value != null) {
+            for (i in itemListLiveData.value!!) {
+                repository.addItem(Item(0, i.code, formulaName, i.material, i.amount))
             }
         }
     }
+
     fun addItem(item: Item) = repository.addItem(item)
 
     fun addItemToList(item: NewItem) {
-        if (itemListLiveData.value == null){
+        if (itemListLiveData.value == null) {
             itemListLiveData.value = mutableListOf(item)
-        }else{
+        } else {
             val new = itemListLiveData.value
             if (new != null) {
                 new.add(item)
@@ -67,8 +73,10 @@ class FormulaViewModel(application: Application) : AndroidViewModel(application)
             //itemListLiveData.value = itemListLiveData.value?.plus(item)
         }
     }
+
     fun updateItem(item: Item) = repository.updateItem(item)
 
 }
-data class NewItem(var code :String,var material:String, var amount : Double)
+
+data class NewItem(var code: String, var material: String, var amount: Double)
 
