69b8662a926429c719868bd15c30e322994d1fb4
Nick Gharizadeh <nickgharizadeh@gmail.com>
2022-07-03 14:07:18 +0430

    fix/ handle back in web view

--- app/src/main/java/com/example/pottery/ui/WebViewFragment.kt
+++ app/src/main/java/com/example/pottery/ui/WebViewFragment.kt
@@ -3,9 +3,8 @@ package com.example.pottery.ui
 import android.annotation.SuppressLint
 import android.os.Build
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.webkit.WebSettings
 import android.webkit.WebViewClient
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
@@ -37,7 +36,16 @@ class WebViewFragment : Fragment() {
         binding.webView.webViewClient = WebViewClient()
         binding.webView.apply {
             this.loadUrl(formulaViewModel.webViewURL)
-            settings.javaScriptEnabled = true
         }
+        binding.webView.settings.javaScriptEnabled = true
+        val webSettings: WebSettings = binding.webView.settings
+        binding.webView.canGoBack()
+        binding.webView.setOnKeyListener(View.OnKeyListener { _, keyCode, event ->
+            if (keyCode == KeyEvent.KEYCODE_BACK && event.action === MotionEvent.ACTION_UP && binding.webView.canGoBack()) {
+                binding.webView.goBack()
+                return@OnKeyListener true
+            }
+            false
+        })
     }
 }
