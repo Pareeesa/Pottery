2e25609509eba9fb1565abfc18d0c5b385115829
Pareeesa <parisa.faridi80@gmail.com>
2022-07-09 12:08:31 +0430

    Add permissions for camera access

--- app/src/main/java/com/example/pottery/ui/AddFormulaFragment.kt
+++ app/src/main/java/com/example/pottery/ui/AddFormulaFragment.kt
@@ -1,15 +1,10 @@
 package com.example.pottery.ui
 
 import android.Manifest
-import android.app.Activity.*
-import android.content.Context
-import android.content.Intent
+import android.app.Activity.MODE_PRIVATE
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.net.Uri
 import android.os.Bundle
-import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -17,25 +12,18 @@ import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.result.launch
-import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.test.core.app.ApplicationProvider
-import com.bumptech.glide.Glide
-import com.bumptech.glide.load.resource.bitmap.CenterCrop
-import com.bumptech.glide.load.resource.bitmap.RoundedCorners
-import com.bumptech.glide.request.RequestOptions
 import com.example.pottery.R
 import com.example.pottery.adapters.NewItemAdapter
 import com.example.pottery.databinding.FragmentAddFormulaBinding
 import com.example.pottery.room.Formula
 import com.example.pottery.viewModels.FormulaViewModel
 import com.example.pottery.viewModels.NewItem
-import java.io.ByteArrayOutputStream
-import java.io.File
 import java.io.IOException
 import java.util.*
 
