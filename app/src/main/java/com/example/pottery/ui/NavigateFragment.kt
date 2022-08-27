fe4c9d516873358f9b65f08ec2a0f5a5a5acccb8
Parisa Faridi <parisa.faridi80@gmail.com>
2022-08-27 17:27:59 +0430

    .

--- app/src/main/java/com/example/pottery/ui/NavigateFragment.kt
+++ app/src/main/java/com/example/pottery/ui/NavigateFragment.kt
@@ -2,13 +2,18 @@ package com.example.pottery.ui
 
 import android.app.Activity
 import android.app.Activity.RESULT_OK
+import android.content.ContentUris
+import android.content.Context
 import android.content.Intent
+import android.database.Cursor
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.os.Environment
+import android.provider.DocumentsContract
+import android.provider.MediaStore
 import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
@@ -38,8 +43,10 @@ class NavigateFragment : Fragment() {
     private lateinit var textFilePath: String
     private lateinit var imagesPath: String
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?): View {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         binding = FragmentNavigateBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -101,9 +108,17 @@ class NavigateFragment : Fragment() {
                 getBackup()
             }
         }
-
         binding.button2.setOnClickListener {
-            openDialog()
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R && !Environment.isExternalStorageManager()) {
+                getAccessibility()
+                Toast.makeText(
+                    requireContext(),
+                    "لطفا ابتدا دسترسی مربوطه را به نرم افزار بدهید و سپس دوباره نسخه پشتیبان تهیه کنید",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                openDialog()
+            }
         }
     }
 
@@ -127,16 +142,17 @@ class NavigateFragment : Fragment() {
             }
             .execute()
         val files = context?.filesDir?.listFiles()
-        for (i in formulaViewModel.getAllFormulas()!!){
-            files?.filter { it.canRead() && it.isFile && it.name.endsWith(".jpg") && it.name=="${i.imagePath}.jpg"  }
+        for (i in formulaViewModel.getAllFormulas()!!) {
+            files?.filter { it.canRead() && it.isFile && it.name.endsWith(".jpg") && it.name == "${i.imagePath}.jpg" }
                 ?.map {
                     val bytes = it.readBytes()
                     val bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
-                    saveImageToExternal(bmp,i.imagePath)
+                    saveImageToExternal(bmp, i.imagePath)
                 }
         }
     }
-    private fun saveImageToExternal(finalBitmap: Bitmap,imagePath:String) {
+
+    private fun saveImageToExternal(finalBitmap: Bitmap, imagePath: String) {
         val myDir = File("$dir/images")
         if (!myDir.exists()) {
             myDir.mkdirs()
@@ -153,16 +169,17 @@ class NavigateFragment : Fragment() {
             e.printStackTrace()
         }
     }
-    private fun saveBackUpImagesToInternalStorage(){
+
+    private fun saveBackUpImagesToInternalStorage() {
         val file = File("$dir/$imagesPath")
         val images = file.listFiles()
-        for (image in images!!){
+        for (image in images!!) {
             try {
-                context?.openFileOutput("${image.name}.jpg", Activity.MODE_PRIVATE).use { stream->
-                    if (!(image as Bitmap).compress(Bitmap.CompressFormat.JPEG,95,stream))
+                context?.openFileOutput("${image.name}.jpg", Activity.MODE_PRIVATE).use { stream ->
+                    if (!(image as Bitmap).compress(Bitmap.CompressFormat.JPEG, 95, stream))
                         throw IOException("COULDN'T SAVE BITMAP")
                 }
-            }catch (e:IOException){
+            } catch (e: IOException) {
                 e.printStackTrace()
             }
         }
@@ -182,13 +199,13 @@ class NavigateFragment : Fragment() {
         saveBackUpImagesToInternalStorage()
     }
 
-    private fun openDialog() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            val i = Intent(Intent.ACTION_OPEN_DOCUMENT_TREE)
-            i.addCategory(Intent.CATEGORY_DEFAULT)
-            startActivityForResult(Intent.createChooser(i, "Choose directory"), 9999)
-        }
+    fun openDialog() {
+        val intent = Intent()
+            .setType("*/*")
+            .setAction(Intent.ACTION_GET_CONTENT)
+        startActivityForResult(Intent.createChooser(intent, "Select a file"), 111)
     }
+
     @Deprecated("Deprecated in Java")
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
@@ -197,14 +214,175 @@ class NavigateFragment : Fragment() {
             val files = directory?.listFiles()
             val txtFile = files?.get(0)
             val images = files?.get(1)
-            if (txtFile?.name == "TooskaWoodBackupFile.txt" && images?.name == "images"){
+            if (txtFile?.name == "TooskaWoodBackupFile.txt" && images?.name == "images") {
                 textFilePath = txtFile.uri.path.toString().split(":")[2]
                 imagesPath = images.uri.path.toString().split(":")[2]
-                restoreBackup()
-            }else{
-                Toast.makeText(requireContext(), "فایل مورد نظر یافت نشد!", Toast.LENGTH_SHORT).show()
-            }
+                DocumentsContract.getTreeDocumentId(data.data)
 
+                //restoreBackup()
+            } else {
+                Toast.makeText(requireContext(), "فایل مورد نظر یافت نشد!", Toast.LENGTH_SHORT)
+                    .show()
+            }
         }
     }
+
+    fun getRealPathFromURI(context: Context, uri: Uri): String? {
+        when {
+            // DocumentProvider
+            DocumentsContract.isDocumentUri(context, uri) -> {
+                when {
+                    // ExternalStorageProvider
+                    isExternalStorageDocument(uri) -> {
+                        val docId = DocumentsContract.getDocumentId(uri)
+                        val split = docId.split(":").toTypedArray()
+                        val type = split[0]
+                        // This is for checking Main Memory
+                        return if ("primary".equals(type, ignoreCase = true)) {
+                            if (split.size > 1) {
+                                Environment.getExternalStorageDirectory()
+                                    .toString() + "/" + split[1]
+                            } else {
+                                Environment.getExternalStorageDirectory().toString() + "/"
+                            }
+                            // This is for checking SD Card
+                        } else {
+                            "storage" + "/" + docId.replace(":", "/")
+                        }
+                    }
+                    isDownloadsDocument(uri) -> {
+                        val fileName = getFilePath(context, uri)
+                        if (fileName != null) {
+                            return Environment.getExternalStorageDirectory()
+                                .toString() + "/Download/" + fileName
+                        }
+                        var id = DocumentsContract.getDocumentId(uri)
+                        if (id.startsWith("raw:")) {
+                            id = id.replaceFirst("raw:".toRegex(), "")
+                            val file = File(id)
+                            if (file.exists()) return id
+                        }
+                        val contentUri = ContentUris.withAppendedId(
+                            Uri.parse("content://downloads/public_downloads"),
+                            java.lang.Long.valueOf(id)
+                        )
+                        return getDataColumn(context, contentUri, null, null)
+                    }
+                    isMediaDocument(uri) -> {
+                        val docId = DocumentsContract.getDocumentId(uri)
+                        val split = docId.split(":").toTypedArray()
+                        val type = split[0]
+                        var contentUri: Uri? = null
+                        when (type) {
+                            "image" -> {
+                                contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                            }
+                            "video" -> {
+                                contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI
+                            }
+                            "audio" -> {
+                                contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
+                            }
+                        }
+                        val selection = "_id=?"
+                        val selectionArgs = arrayOf(split[1])
+                        return getDataColumn(context, contentUri, selection, selectionArgs)
+                    }
+                }
+            }
+            "content".equals(uri.scheme, ignoreCase = true) -> {
+                // Return the remote address
+                return if (isGooglePhotosUri(uri)) uri.lastPathSegment else getDataColumn(
+                    context,
+                    uri,
+                    null,
+                    null
+                )
+            }
+            "file".equals(uri.scheme, ignoreCase = true) -> {
+                return uri.path
+            }
+        }
+        return null
+    }
+
+    private fun getDataColumn(
+        context: Context, uri: Uri?, selection: String?,
+        selectionArgs: Array<String>?
+    ): String? {
+        var cursor: Cursor? = null
+        val column = "_data"
+        val projection = arrayOf(
+            column
+        )
+        try {
+            if (uri == null) return null
+            cursor = context.contentResolver.query(
+                uri, projection, selection, selectionArgs,
+                null
+            )
+            if (cursor != null && cursor.moveToFirst()) {
+                val index = cursor.getColumnIndexOrThrow(column)
+                return cursor.getString(index)
+            }
+        } finally {
+            cursor?.close()
+        }
+        return null
+    }
+
+
+    private fun getFilePath(context: Context, uri: Uri?): String? {
+        var cursor: Cursor? = null
+        val projection = arrayOf(
+            MediaStore.MediaColumns.DISPLAY_NAME
+        )
+        try {
+            if (uri == null) return null
+            cursor = context.contentResolver.query(
+                uri, projection, null, null,
+                null
+            )
+            if (cursor != null && cursor.moveToFirst()) {
+                val index = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DISPLAY_NAME)
+                return cursor.getString(index)
+            }
+        } finally {
+            cursor?.close()
+        }
+        return null
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is ExternalStorageProvider.
+     */
+    private fun isExternalStorageDocument(uri: Uri): Boolean {
+        return "com.android.externalstorage.documents" == uri.authority
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is DownloadsProvider.
+     */
+    private fun isDownloadsDocument(uri: Uri): Boolean {
+        return "com.android.providers.downloads.documents" == uri.authority
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is MediaProvider.
+     */
+    private fun isMediaDocument(uri: Uri): Boolean {
+        return "com.android.providers.media.documents" == uri.authority
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is Google Photos.
+     */
+    private fun isGooglePhotosUri(uri: Uri): Boolean {
+        return "com.google.android.apps.photos.content" == uri.authority
+    }
 }
+
